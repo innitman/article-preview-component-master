d0ffea5d8f7cc6fff46f4b2570785d870c047e0d
innitman <viral.thakerar@gmail.com>
2024-08-16 20:29:18 +0100

    tried

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,11 @@
-let cardFooterButton = document.querySelector(".card-footer-button");
-let cardFooterUnexpanded = document.querySelector(".card-footer-not-expanded");
-let cardFooterSocial = document.querySelector(".card-footer-social-expanded");
+let cardFooterButtons = document.querySelectorAll(".card-footer-button");
+let cardFooterSocialAdditional = document.querySelector(
+  ".card-footer-social-expanded"
+);
 
-cardFooterButton.addEventListener("click", () => {
-  cardFooterUnexpanded.classList.toggle("hidden");
-  cardFooterSocial.classList.toggle("hidden");
+cardFooterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    cardFooterSocialAdditional.classList.toggle("hidden");
+    button.classList.toggle("top-8px");
+  });
 });
